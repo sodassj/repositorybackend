32020bd103318d1ddc44568111c0c7afde5dc663
David Maldonado Pizarro <jose.maldonado.p@tecsup.edu.pe>
2025-05-11 18:35:53 -0500

    Fix: usar 0.0.0.0 y enviar respuesta HTTP

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const http = require('http')
 
-function requestController(){
-  console.log('Bienvenidos al curso')
+function requestController(req, res) {
+  console.log('Bienvenidos al curso');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bienvenidos al curso');
 }
 
 const server = http.createServer(requestController)
 
 const PORT = process.env.PORT || 4000;
 
-server.listen(PORT, function (){
-  console.log("Aplicación corriendo en: " + PORT)
+server.listen(PORT, '0.0.0.0', function () {
+  console.log("Aplicación corriendo en: " + PORT);
 });
